f44248291b4b90a2de936895e036ab01cd11d143
Markadiusz <arkadiusz.czarkowski2212@gmail.com>
2022-05-31 23:30:35 +0200

    Reorganised code; board and pieces display properly

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,218 @@ use bevy::prelude::*;
 
 const IMAGE_SIZE: (f32, f32) = (45., 45.);
 
-fn create_board(
+const TILEL_SPRITE: &str = "sprites/tilel.png";
+const KINGL_SPRITE: &str = "sprites/kl.png";
+const QUEENL_SPRITE: &str = "sprites/ql.png";
+const ROOKL_SPRITE: &str = "sprites/rl.png";
+const BISHOPL_SPRITE: &str = "sprites/bl.png";
+const KNIGHTL_SPRITE: &str = "sprites/nl.png";
+const PAWNL_SPRITE: &str = "sprites/pl.png";
+const TILED_SPRITE: &str = "sprites/tiled.png";
+const KINGD_SPRITE: &str = "sprites/kd.png";
+const QUEEND_SPRITE: &str = "sprites/qd.png";
+const ROOKD_SPRITE: &str = "sprites/rd.png";
+const BISHOPD_SPRITE: &str = "sprites/bd.png";
+const KNIGHTD_SPRITE: &str = "sprites/nd.png";
+const PAWND_SPRITE: &str = "sprites/pd.png";
+
+struct GameTextures {
+    tilel: Handle<Image>,
+    kingl: Handle<Image>,
+    queenl: Handle<Image>,
+    rookl: Handle<Image>,
+    bishopl: Handle<Image>,
+    knightl: Handle<Image>,
+    pawnl: Handle<Image>,
+    tiled: Handle<Image>,
+    kingd: Handle<Image>,
+    queend: Handle<Image>,
+    rookd: Handle<Image>,
+    bishopd: Handle<Image>,
+    knightd: Handle<Image>,
+    pawnd: Handle<Image>,
+}
+
+enum PieceColor {
+    White,
+    Black,
+}
+
+enum PieceType {
+    King,
+    Queen,
+    Bishop,
+    Knight,
+    Rook,
+    Pawn,
+}
+
+#[derive(Component)]
+struct Piece {
+    color: PieceColor,
+    piece_type: PieceType,
+    x: u8,
+    y: u8,
+}
+
+fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut materials: Res<Assets<ColorMaterial>>,
-    mut windows: ResMut<Windows>,
 ) {
+    let game_textures = GameTextures {
+        tilel: asset_server.load(TILEL_SPRITE),
+        kingl: asset_server.load(KINGL_SPRITE),
+        queenl: asset_server.load(QUEENL_SPRITE),
+        rookl: asset_server.load(ROOKL_SPRITE),
+        bishopl: asset_server.load(BISHOPL_SPRITE),
+        knightl: asset_server.load(KNIGHTL_SPRITE),
+        pawnl: asset_server.load(PAWNL_SPRITE),
+        tiled: asset_server.load(TILED_SPRITE),
+        kingd: asset_server.load(KINGD_SPRITE),
+        queend: asset_server.load(QUEEND_SPRITE),
+        rookd: asset_server.load(ROOKD_SPRITE),
+        bishopd: asset_server.load(BISHOPD_SPRITE),
+        knightd: asset_server.load(KNIGHTD_SPRITE),
+        pawnd: asset_server.load(PAWND_SPRITE),
+    };
+    commands.insert_resource(game_textures);
+
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
 
-    let mut window = windows.get_primary_mut().unwrap();
-    window.set_position(IVec2::new(1000, 1000));
-
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("sprites/pl.png"),
-        ..Default::default()
-    });
-
-    let tilel = asset_server.load("sprites/tilel.png");
-    let tiled = asset_server.load("sprites/tiled.png");
-
+fn create_board(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+) {
+    let tilel = game_textures.tilel.clone();
+    let tiled = game_textures.tiled.clone();
     for i in 0..8 {
         for j in 0..8 {
-            let tile = if (i + j) % 2 == 0 { &tilel } else { &tiled };
-            spawn_tile(&mut commands, tile.clone(), Vec3::new(i as f32 * IMAGE_SIZE.0, j as f32 * IMAGE_SIZE.1, 10.));
+            let tile = if (i + j) % 2 == 0 { &tiled } else { &tilel };
+            spawn_tile(
+                &mut commands,
+                tile.clone(),
+                Vec3::new(i as f32 * IMAGE_SIZE.0, j as f32 * IMAGE_SIZE.1, 0.)
+            );
         }
+    }
+}
+
+fn create_pieces(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+) {
+    // kingl
+    spawn_piece(
+        &mut commands,
+        game_textures.kingl.clone(),
+        Vec3::new(4. * IMAGE_SIZE.0, 0., 1.),
+        PieceColor::White,
+        PieceType::King,
+    );
+    // queenl
+    spawn_piece(
+        &mut commands,
+        game_textures.queenl.clone(),
+        Vec3::new(3. * IMAGE_SIZE.0, 0., 1.),
+        PieceColor::White,
+        PieceType::Queen,
+    );
+    // rookl
+    for i in vec![0., 7.] {
+        spawn_piece(
+            &mut commands,
+            game_textures.rookl.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, 0., 1.),
+            PieceColor::White,
+            PieceType::Rook,
+        );
+    }
+    // bishopl
+    for i in vec![2., 5.] {
+        spawn_piece(
+            &mut commands,
+            game_textures.bishopl.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, 0., 1.),
+            PieceColor::White,
+            PieceType::Bishop,
+        );
+    }
+    // knightl
+    for i in vec![1., 6.] {
+        spawn_piece(
+            &mut commands,
+            game_textures.knightl.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, 0., 1.),
+            PieceColor::White,
+            PieceType::Knight,
+        );
+    }
+    // pawnl
+    for i in 0..8 {
+        spawn_piece(
+            &mut commands,
+            game_textures.pawnl.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, IMAGE_SIZE.1, 1.),
+            PieceColor::White,
+            PieceType::Pawn,
+        );
+    }
+    // kingd
+    spawn_piece(
+        &mut commands,
+        game_textures.kingd.clone(),
+        Vec3::new(4. * IMAGE_SIZE.0, 7. * IMAGE_SIZE.1, 1.),
+        PieceColor::White,
+        PieceType::King,
+    );
+    // queend
+    spawn_piece(
+        &mut commands,
+        game_textures.queend.clone(),
+        Vec3::new(3. * IMAGE_SIZE.0, 7. * IMAGE_SIZE.1, 1.),
+        PieceColor::Black,
+        PieceType::Queen,
+    );
+    // rookd
+    for i in vec![0., 7.] {
+        spawn_piece(
+            &mut commands,
+            game_textures.rookd.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, 7. * IMAGE_SIZE.1, 1.),
+            PieceColor::Black,
+            PieceType::Rook,
+        );
+    }
+    // bishopd
+    for i in vec![2., 5.] {
+        spawn_piece(
+            &mut commands,
+            game_textures.bishopd.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, 7. * IMAGE_SIZE.1, 1.),
+            PieceColor::Black,
+            PieceType::Bishop,
+        );
+    }
+    // knightd
+    for i in vec![1., 6.] {
+        spawn_piece(
+            &mut commands,
+            game_textures.knightd.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, 7. * IMAGE_SIZE.1, 1.),
+            PieceColor::Black,
+            PieceType::Knight,
+        );
+    }
+    // pawnd
+    for i in 0..8 {
+        spawn_piece(
+            &mut commands,
+            game_textures.pawnd.clone(),
+            Vec3::new(i as f32 * IMAGE_SIZE.0, 6. * IMAGE_SIZE.1, 1.),
+            PieceColor::Black,
+            PieceType::Pawn,
+        );
     }
 }
 
@@ -44,22 +232,33 @@ fn spawn_tile(
     });
 }
 
-/*fn spawn_pawn(
+fn spawn_piece(
     commands: &mut Commands,
-    color: Color,
-    pawn_handle: Handle<Image>,
-    position: Vec2,
-    ) {
-    let pawn_image = asset_server.load("sprites/pl.svg");
-    /*commands
-        .spawn_bundle(SpriteBundle {
-            sprite: 
-            */
-}*/
+    texture: Handle<Image>,
+    position: Vec3,
+    color: PieceColor,
+    piece_type: PieceType,
+) {
+    commands.spawn_bundle(SpriteBundle {
+        texture,
+        transform: Transform {
+            translation: position,
+            ..Default::default()
+        },
+        ..Default::default()
+    }).insert(Piece {
+        color,
+        piece_type,
+        x: position.x as u8,
+        y: position.y as u8,
+    });
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(create_board)
+        .add_startup_system(setup)
+        .add_startup_system_to_stage(StartupStage::PostStartup, create_board)
+        .add_startup_system_to_stage(StartupStage::PostStartup, create_pieces)
         .run();
 }
